b6db175a723a20ed56788e870e564f434b35cc36
Shodhan-shetty-udupi <shodhan.shetty@trevonix.com>
2025-03-19 00:23:28 +0530

    Updated main.tf file

--- Import/tmp/main.tf
+++ Import/tmp/main.tf
@@ -7,9 +7,5 @@ terraform {
   }
 }
 
-provider "auth0" {
-  domain       = "main-tenant.cic-demo-platform.auth0app.com"
-  client_id    = "BYie4ua8oR6rRIV4aePhZ3z7O9gDDedJ"
-  client_secret = "_4PPgjE87aLWLnCEwWYThxRM-A2lekJ8Njzz8kZutUm_pR-8Xilx1SZxLQZV9zpb"
-}
+provider "auth0" {}
 
