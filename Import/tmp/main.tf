f09b17f28194e3d623c64e46e6ef1e981d983a69
Shodhan-shetty-udupi <shodhan.shetty@trevonix.com>
2025-03-18 18:13:30 +0530

    added main.tf file

---
+++ Import/tmp/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = ">= 1.0.0" # Refer to docs for latest version
+    }
+  }
+}
+
+provider "auth0" {
+  domain       = "main-tenant.cic-demo-platform.auth0app.com"
+  client_id    = "BYie4ua8oR6rRIV4aePhZ3z7O9gDDedJ"
+  client_secret = "_4PPgjE87aLWLnCEwWYThxRM-A2lekJ8Njzz8kZutUm_pR-8Xilx1SZxLQZV9zpb"
+}
+
