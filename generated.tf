bbfeb2cb0ec6da543157d3c0ef0fb6aeb52b39cf
Shodhan-shetty-udupi <shodhan.shetty@trevonix.com>
2025-05-09 12:40:28 +0530

    addded app details

--- generated.tf
+++ generated.tf
@@ -1,12 +1,12 @@
 # __generated__ by Terraform
 # Please review these resources and move them into your main configuration files.
 
-# __generated__ by Terraform from "k6tFUs9RwOkJ5GdplUG6Pa0QTOBpli80"
-resource "auth0_client" "default_app" {
+# __generated__ by Terraform from "Ra2yoYu8Q60EzrimIe2g8G5CNHqeq4wh"
+resource "auth0_client" "regular_app" {
   allowed_clients                       = []
   allowed_logout_urls                   = []
   allowed_origins                       = []
-  app_type                              = null
+  app_type                              = "regular_web"
   callbacks                             = var.callbacks
   client_aliases                        = []
   client_metadata                       = {}
@@ -16,13 +16,14 @@ resource "auth0_client" "default_app" {
   custom_login_page                     = null
   custom_login_page_on                  = true
   description                           = null
+  encryption_key                        = null
   form_template                         = null
   grant_types                           = ["authorization_code", "implicit", "refresh_token", "client_credentials"]
   initiate_login_uri                    = null
   is_first_party                        = true
   is_token_endpoint_ip_header_trusted   = false
   logo_uri                              = null
-  name                                  = "Default App by terraform jenkins"
+  name                                  = "Regular App via terraform and jenkins."
   oidc_conformant                       = true
   organization_require_behavior         = null
   organization_usage                    = null
@@ -42,13 +43,24 @@ resource "auth0_client" "default_app" {
     scopes              = {}
     secret_encoded      = false
   }
+  native_social_login {
+    apple {
+      enabled = false
+    }
+    facebook {
+      enabled = false
+    }
+    google {
+      enabled = false
+    }
+  }
   refresh_token {
     expiration_type              = "non-expiring"
-    idle_token_lifetime          = 1296000
+    idle_token_lifetime          = 2592000
     infinite_idle_token_lifetime = true
     infinite_token_lifetime      = true
     leeway                       = 0
     rotation_type                = "non-rotating"
-    token_lifetime               = 2592000
+    token_lifetime               = 31557600
   }
 }
